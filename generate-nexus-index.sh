4cee2d184ffcf339a81a10f6ae52338a7ff1b2bb
Toby Weston <toby.weston@gmail.com>
2012-01-28 18:01:52 +0000

    moving nexus index data

--- generate-nexus-index.sh
+++
@@ -1,1 +0,0 @@
-java -jar nexus-indexer-3.0.4-cli.jar -t full -i nexus-index -n robotooling -r maven/ -s
---
+++ update-nexus-index.sh
@@ -0,0 +1,1 @@
+java -jar nexus-indexer-3.0.4-cli.jar -t full -i nexus-index -n robotooling -d maven -r maven/ -s
