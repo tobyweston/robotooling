27a9f5b8ae1a30f4f227795ca41217fe32140202
Toby Weston <toby@larma.local>
2012-03-18 15:35:18 +0000

    tweat to directory index sh

--- update-directory-indeces.sh
+++ update-directory-indeces.sh
@@ -2,7 +2,7 @@
  
 for DIR in $(find ./maven -type d); do
   (
-    echo -e "<html>\n<head>\n<link rel="stylesheet" href="http://robotooling.com/basic.css">\n<script src="http://robotooling.com/analytics.js"></script>\n</head><body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+    echo -e "<html>\n<head>\n<link rel="stylesheet" href="http://robotooling.com/basic.css">\n<script src="http://robotooling.com/analytics.js"></script>\n</head><body>\n<h1>bad.robot.repo - listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
