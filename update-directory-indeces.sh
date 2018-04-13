71014a7a5265c8aaf22f4290aa6fc1d81ac38dd7
Toby <toby.weston@gmail.com>
2018-04-13 17:13:15 +0100

    updated to generate index pages for the debian repo

--- update-directory-indeces.sh
+++ update-directory-indeces.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-for DIR in $(find ./maven -type d); do
+for DIR in $(find ./maven -type d && find ./debian -type d); do
   (
     echo -e "<html>\n<head>\n<link rel="stylesheet" href="http://robotooling.com/basic.css">\n<script src="http://robotooling.com/analytics.js"></script>\n</head><body>\n<h1>bad.robot.repo - listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
