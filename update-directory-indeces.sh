1b1ff8af6b9744706f570ac0077b0fd25d042c45
Toby <toby.weston@gmail.com>
2018-05-10 11:35:19 +0100

    add timestamp for files

--- update-directory-indeces.sh
+++ update-directory-indeces.sh
@@ -2,8 +2,28 @@
  
 for DIR in $(find ./maven -type d && find ./debian -type d); do
   (
+    DATE=`date '+%Y-%m-%d %H:%M:%S'`
+    FILES=`ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$"`
+
     echo -e "<html>\n<head>\n<link rel="stylesheet" href="http://robotooling.com/basic.css">\n<script src="http://robotooling.com/analytics.js"></script>\n</head><body>\n<h1>bad.robot.repo - listing</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+    for FILE in $FILES; do
+    
+        # exclude folders
+        
+        if  [ -d "${FILE}" ] ; then
+            TIMESTAMP=""
+        else
+            FILE_COUNT=`ls -lAT ${DIR}/${FILE} | wc -l | tr -d '[:space:]'`
+            if [ "${FILE_COUNT}" == "1" ] ;then
+                TIMESTAMP=`ls -lAT ${DIR}/${FILE} | awk '{ printf "%s-%s-%s %s",$6,$7,$9,$8 }'`
+            else
+                TIMESTAMP=""
+            fi
+        fi
+        
+        printf "<a href=\"%s\">%s</a> <span>%s</span>\n" "${FILE}" "${FILE}" "${TIMESTAMP}"
+    done
+
     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
